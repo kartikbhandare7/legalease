0adfc7007da0f270580b909f821ed465a9a3e828
Kartik Bhandare <kartikbhandare57@gmail.com>
2026-07-20 18:06:48 +0530

    Initial commit

---
+++ src/main/java/com/legalease/LegaleaseApplication.java
@@ -0,0 +1,13 @@
+package com.legalease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegaleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegaleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legalease/LegaleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legalease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegaleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
