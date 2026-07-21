b273648908ec9cb4fe64df06e4055e2f49393003
Kartik Bhandare <kartikbhandare57@gmail.com>
2026-07-21 16:20:06 +0530

    initialzes jwt tokens verification

---
+++ src/main/java/com/legalease/common/security/JwtService.java
@@ -0,0 +1,66 @@
+package com.legalease.common.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${app.jwt.secret")
+    private String jwtSecret;
+
+    @Value("${app.jwt.expiration-ms")
+    private long jwtExpirationMs;
+
+    public String generateToken(String email, String role, UUID userId){
+        return Jwts.builder()
+                .subject(email)
+                .claims(Map.of(
+                        "role", role,
+                        "userId", userId.toString()
+                ))
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractEmail(String token){
+        return extractClaim(token , Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token , Function<Claims, T> claimsResolver){
+        return claimsResolver.apply(extractAllClaims(token));
+    }
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        String email = extractEmail(token);
+        return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractClaim(token , Claims::getExpiration).before(new Date());
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+}
---
+++ src/main/java/com/legalease/user/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package com.legalease.user.repository;
+
+public class UserRepository {
+}
